85ed4963aea1f56df7f6526ece4c7c79b2bd25eb
Rohit Pai <rohit23110@gmail.com>
2020-06-14 21:20:45 +0530

    Created basic matching function

---
+++ Matching.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+if __name__ == "__main__":
+    sellorders = pd.read_csv("Sellordersnew.csv")
+    buyorders = pd.read_csv("Buyordersnew.csv")
+    transactions = pd.DataFrame(columns = ['buy_customer_id', 'sell_customer_id', 'quantity', 'stock_code', 'price', 'buy_order_id', 'sell_order_id'])
+
+    for i in range(buyorders.shape[0]):
+        sellorder = sellorders[(sellorders.status == "Pending") & (buyorders.iloc[i].status == "Pending") & (sellorders.stock_code == buyorders.iloc[i].stock_code) & (sellorders.quantity == buyorders.iloc[i].quantity)]
+        if len(sellorder) != 0:
+            sellorder = sellorder.iloc[0]
+            print(sellorder, "\n")
+            # print(sellorder.order_id, "\n")
+            transaction = {'buy_customer_id': buyorders.iloc[i].customer_id, 'sell_customer_id': sellorder.customer_id, 'quantity': sellorder.quantity, 'stock_code' : sellorder.stock_code, 'price': 1, 'buy_order_id': buyorders.iloc[i].order_id, 'sell_order_id': sellorder.order_id}
+            transactions = transactions.append(transaction, ignore_index=True)
+            sellorders.loc[sellorders.order_id == sellorder.order_id, 'status'] = "Completed"
+            buyorders.at[i, 'status'] = "Completed"
+    # print(buyorders)
+    # print(sellorders)
+    print(transactions)
+    buyorders.to_csv("Buyordersnew.csv", sep=",", index=False)
+    sellorders.to_csv("Sellordersnew.csv", sep=",", index=False)
