fe0941e5000f6606da22db76745c539f827dfcd0
Rohit Pai <rohit23110@gmail.com>
2020-06-21 16:42:58 +0530

    Fixed error where transactions got converted to float

--- Matching.py
+++ Matching.py
@@ -1,11 +1,19 @@
 import pandas as pd
 from Output import *
 from fetchPrices import getCurrentPrice
-# if __name__ == "__main__":
-def matching():
+if __name__ == "__main__":
+# def matching():
     sellorders = pd.read_csv("Sellorders.csv")
+    # print(sellorders.dtypes)
+    # sellorders.limit_price = sellorders.limit_price.fillna(value="")
+    # print(sellorders.dtypes)
     buyorders = pd.read_csv("Buyorders.csv")
+    # print(buyorders.dtypes)
+    # buyorders.limit_price = buyorders.limit_price.fillna(value="")
+    # print(buyorders.dtypes)
     transactions = pd.DataFrame(columns = ['buy_customer_id', 'sell_customer_id', 'quantity', 'stock_code', 'price', 'buy_order_id', 'sell_order_id'])
+    # transactions.price = transactions.price.astype("float")
+    # print(transactions.dtypes)
 
     for i in range(buyorders.shape[0]):
         if buyorders.iloc[i].order_type == 'market':
@@ -30,8 +38,23 @@ def matching():
                 buyorders.at[i, 'status'] = "completed"
 
     # print(buyorders)
+    # print(buyorders.dtypes)
     # print(sellorders)
+    # print(sellorders.dtypes)
     # print(transactions)
+    # print(transactions.dtypes)
+    convert_dict = { # Had to add this to prevent the colums from converting to float
+        "buy_customer_id": "int64",
+        "sell_customer_id": "int64",
+        "quantity": "int64",
+        "stock_code": "int64",
+        "price": "float64",
+        "buy_order_id": "int64",
+        "sell_order_id": "int64"
+    }
+    transactions = transactions.astype(convert_dict)
+    # print(transactions)
+    # print(transactions.dtypes)
     printOutput(sellorders,buyorders,transactions)
     buyorders.to_csv("Buyorders.csv", sep=",", na_rep="NaN", index=False)
     sellorders.to_csv("Sellorders.csv", sep=",", na_rep="NaN", index=False)
