a9c4a2f95f1fed2000a62a28beb810955a01e29d
Rohit Pai <rohit23110@gmail.com>
2020-06-25 19:26:02 +0530

    Working code with 1-2 tasks remaining

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ from flask import Flask,request,jsonify,url_for,render_template,redirect,logging
 import json
 import pandas as pd
 import csv
-import schedule
-import time
 import datetime
+import subprocess
 from csv import DictWriter
 from forms import inputOrderForm
 from Matching import matching
-from Cancellation import remove
 
+subprocess.call('python Cancellation.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
@@ -26,7 +25,6 @@ def show_transaction():
         transaction_data.append(row)
     return render_template('transaction.html',transaction_data = transaction_data)
 
-#@app.route('/place_orders',methods=['GET','POST'])
 @app.route('/',methods=['GET','POST'])
 def get_orders():
     form = inputOrderForm()
@@ -38,23 +36,12 @@ def get_orders():
     for row in input_file:
         if(max_buyorder_id < int(row['order_id'])):
           max_buyorder_id = int(row['order_id'])
-        # row['price'] = getCurrentPrice(row['stock_code'])
-        # print(row)
         buy_data.append(row)
     input_file = csv.DictReader(open("Sellorders.csv"))
     for row in input_file:
         if(max_sellorder_id < int(row['order_id'])):
           max_sellorder_id = int(row['order_id'])
-        # row['price'] = getCurrentPrice(row['stock_code'])
-        # print(row)
         sell_data.append(row)
-    # print(type(buy_data[0]))
-    # buy_data = sorted(buy_data,key= lambda x : x['price'] )
-    # sell_data = sorted(sell_data,key= lambda x : x['price'], reverse = True)
-    # print(buy_data)
-    # print(sell_data)
-    # if(max_order_id == 0):
-    #     max_order_id += 1
     if form.validate_on_submit():
         order_dict = form.data
         trade_type = order_dict['trade_type']
@@ -68,12 +55,12 @@ def get_orders():
         del order_dict['trade_type']
         order_dict['pending_quantity'] = form.quantity.data
         append_dict_as_row(trade_type.title()+'orders.csv',order_dict,['order_id','quantity','pending_quantity','stock_code','customer_id','order_type','flavour','status'])
-        
         currentTime = datetime.datetime.now()
         startTime = datetime.time(9, 15, 0)
         endTime = datetime.time(15, 30, 0)
+        print(currentTime)
 
-        if (currentTime.time() < endTime) and (currentTime.time() > startTime):
+        if ((currentTime.time() < endTime) and (currentTime.time() > startTime)): #works
             matching()
         
         return redirect(url_for('get_orders'))
@@ -81,9 +68,4 @@ def get_orders():
 
 if __name__ == '__main__':
     app.run(debug = True)
-
-schedule.every().day.at("15:30").do(remove) 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
     
